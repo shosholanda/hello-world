221fd0b0c40b6d648dab99b6691e1eae3b3ac055
Davidshiro Pichu <36055257+shosholanda@users.noreply.github.com>
2025-04-07 02:36:02 -0600

    Create init.sh
    Useful commands for fresh install

---
+++ init.sh
@@ -0,0 +1,7 @@
+## Command list for fresh install linux
+sudo apt update
+sudo apt upgrade -y
+sudo apt install git # This must be already installed
+sudo apt install tree neofetch htop default-jdk emacs ssh python3 python3-vev python3-pip okular net-tools fortune cowsay flatpak firefox adb -y
+
+## Create a dynamic setup
