dbc0361412ceecb3ca49de5daa67a0f7749a6fc9
Davidshiro Pichu <davidshiro.pichu@ciencias.unam.mx>
2025-08-12 15:38:02 -0600

    Added pipewire for KDE

--- init.el
+++ init.el
@@ -1,27 +1,21 @@
-;; Mi configuración básica del dios emacs
+;;; Mi configuración básica del dios emacs
 
-;; Display spaces after lines
+;; Mostrar espacios en blanco después de linea
 ;; (setq-default show-trailing-whitespace t)
-;; Display num-line
+;; Mostrar número de línea
 (global-display-line-numbers-mode)
-;; Silly buttons off
+;; Eliminar botones de edición
 (tool-bar-mode 0)
-;; Auto fill mode
+;; Auto fill mode (M-q automático)
+;; Configurar margen: C-x f
 (add-hook 'text-mode-hook 'turn-on-auto-fill)
 ;; BYE BYE Welcome
 (setq inhibit-startup-screen t)
+;; If org-mode doesnt show italics correctly
+(set-face-attribute 'italic nil :family "Hack" :slant 'italic
+:underline nil)
 
-;; Max
-(setq frame-resize-pixelwise t)
 
-(custom-set-variables
- ;; Tema oscuro por mi punto en el ojo
- '(custom-enabled-themes '(deeper-blue))
- ;; FUCK copilot a toda su reputa madre
- '(lsp-copilot-enabled nil)
- '(package-selected-packages
-   '(org-journal htmlize org phps-mode php-mode python-mode treemacs-projectile which-key helm-lsp helm company yasnippet flycheck projectile lsp-ui lsp-java auctex))
- )
 
 ;; Eliminar autoguardados fuera de la carpeta actual
 ;; Write backups to ~/.emacs.d/backup/
@@ -32,72 +26,41 @@
       kept-new-versions      20 ; how many of the newest versions to keep
       kept-old-versions      5) ; and how many of the old
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(deeper-blue))
+ '(package-selected-packages
+   '(org htmlize treemacs-projectile projectile company helm-lsp lsp-ui lsp-mode helm yasnippet-snippets yasnippet flycheck which-key use-package))
+ '(tool-bar-mode nil))
+
+
 ;; MELPA = más paquetes
 (package-initialize)
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
 
-
-;; Project management
-(use-package projectile)
-;; Real-time syntax checking
-(use-package flycheck)
-;; Snippets
-(use-package yasnippet :config (yas-global-mode))
-;; language server protocol support = más soporte de lenguajes
-(use-package lsp-mode :hook ((lsp-mode . lsp-enable-which-key-integration)))
-;; Mostrar keybindings
-(use-package hydra)
-;; textito bonito para lisp
-(use-package lsp-ui)
-;; Posibles combinaciones
+;; Instalar M-x install-package RET use-package
+(require 'use-package)
+;; Mostrar opciones a mitad de comando
 (use-package which-key :config (which-key-mode))
-;; Soporte para java
-(use-package lsp-java :config (add-hook 'java-mode-hook 'lsp))
-;; Helm ??
+;; Mostrar errores de sintaxis, semántica en varios lenguajes
+(use-package flycheck)
+;; (add-hook 'after-init-hook #'global-flycheck-mode)
+;; Autocompletar código.
+(use-package yasnippet :config (yas-global-mode))
+;; Mostrar lista de completaciones
+(use-package company)
+;; (add-hook 'after-init-hook 'global-company-mode)
+;; Autocompletar buffers filtrando resultados
+(use-package helm :config (helm-mode) (global-set-key (kbd "C-x C-f") 'helm-find-files))
+;; WTF
 (use-package helm-lsp)
-;; completar comandos
-(use-package helm
-  :config (helm-mode))
-;; Navegacion fácil
-(use-package lsp-treemacs)
-
-
-
-;; Soporte para python
-(require 'lsp-python-ms)
-(setq lsp-python-ms-auto-install-server t)
-(add-hook 'python-mode-hook #'lsp) ; or lsp-deferred
-
-(use-package lsp-python-ms
-  :hook (python-mode . (lambda ()
-                         (require 'lsp-python-ms)
-                         (lsp))))  ; or lsp-deferred
-
-
-;; Journaling
-(require 'org-journal)
-
-;; Cerrar Diario al guardar
-
-;; (defun org-journal-save-entry-and-exit()
-;;   "Simple convenience function.
-;;   Saves the buffer of the current day's entry and kills the window
-;;   Similar to org-capture like behavior"
-;;   (interactive)
-;;   (save-buffer)
-;;   (kill-buffer-and-window))
-;; (define-key org-journal-mode-map (kbd "C-x C-s") 'org-journal-save-entry-and-exit)
-
-;; Basic notes but instantly
-(setq org-capture-templates
-      '(("n" "Note Entry" entry (file+datetree "~/notes.org")
-         "* %?\n%U"
-         :empty-lines 1)))
-
-;; Quick journal keybinding para notas
-(global-set-key (kbd "C-c n") (lambda () (interactive) (org-capture nil "n")))
-;; Quick keybind para journal
-(global-set-key (kbd "C-c j") 'org-journal-new-entry)
+;; IDE mode
+(use-package lsp-mode :hook ((lsp-mode . lsp-enable-which-key-integration)))
+;; textito bonito para emacs
+(use-package lsp-ui)
 
--- init.sh
+++ init.sh
@@ -1,9 +1,6 @@
 ## Command list for fresh install linux
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install emacs git tree neofetch htop default-jdk ssh
-command-not-found python3-vev python3-pip net-tools brave flatpak
-cowsay toilet 2048 oneko okular firefox adb -y
+sudo apt install emacs git tree neofetch htop default-jdk ssh command-not-found python3-vev python3-pip net-tools brave flatpak cowsay toilet 2048 oneko okular firefox adb pipewire -y
 
-echo "alias ll=\"ls -lah\"" >> ~/.bashrc
-echo "alias apagar=\"shutdown -h now\"" >> ~/.bashrc
+
