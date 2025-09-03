fb764917a26c826e4bc1e7356d5dad34155bdce2
Davidshiro Pichu <36055257+shosholanda@users.noreply.github.com>
2025-09-02 20:27:24 -0600

    Actualizar init.sh
    addad tmux lolcat hyfetch nmap

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 ## Command list for fresh install linux
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install emacs git tree neofetch htop default-jdk ssh command-not-found python3-vev python3-pip net-tools brave flatpak cowsay toilet 2048 oneko okular firefox adb pipewire -y
+sudo apt install emacs git tree hyfetch htop default-jdk ssh command-not-found python3-vev python3-pip net-tools brave flatpak cowsay toilet 2048 oneko okular firefox adb pipewire tmux lolcat nmap -y
 
 
