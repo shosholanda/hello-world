2030dd9da2dc3ad4fa42c6478f661dd7b17e4e93
Davidshiro Pichu <36055257+shosholanda@users.noreply.github.com>
2025-09-25 17:04:10 -0600

    Update init.sh
    Organized packages

--- init.sh
+++ init.sh
@@ -1,6 +1,13 @@
 ## Command list for fresh install linux
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install emacs git tree hyfetch htop default-jdk ssh command-not-found python3-vev python3-pip net-tools brave flatpak cowsay toilet 2048 oneko okular firefox adb pipewire tmux lolcat nmap -y
-
-
+# Linux utilities
+sudo apt install emacs git tree fastfetch htop ssh command-not-found net-tools adb tmux acpi network-manager
+# Languages
+sudo apt install default-jdk python3-venv python3-pip rustc ghc golang
+# Apps
+sudo apt install brave flatpak okular firefox pipewire
+# Games
+sudo apt install cowsay toilet 2048 oneko lolcat
+# Hacking
+sudo apt install bettercap nmap wireshark stegosuit aircrack-ng hashcat
